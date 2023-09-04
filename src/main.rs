c9c5fbfed7251d11f2df52d748195231cb97637d
valselee <sollleky72@gmail.com>
2023-09-04 13:07:50 +0900

    [Chore] 1. main.rs 간단히 수정

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 // directory name == mod
 mod guessing_number;
+mod rust_programming_concepts;
 
 fn main() {
     /* Day 1: Guessing Number Practice */
-    guessing_number::guessing_number_mod::guessing_number();
+    // guessing_number::guessing_number_mod::guessing_number();
+
+    /* Day2: rust_programming_concepts */
+    rust_programming_concepts::rust_programming_concepts::variable_control_shadowing();
+    rust_programming_concepts::rust_programming_concepts::variable_control_mut();
 }
